25a495cbecd7583c8bd86608f3134a41f6b60bd3
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-01 23:49:50 -0500

    started category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,38 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+
+  // on successful request return product categories
+  try {
+    res.status(200).json(await Category.findAll({ include: Product }))
+  } catch (error) {
+    console.error(error) // on bad request return the error
+    res.status(400).json(error)
+  }
+
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+
+  // on successful request find the id of the request in the category models and return the products 
+  try {
+    res.status(200).json(await Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: Product
+    }))
+  } catch (error) {
+    console.log(error)
+    res.status(400)
+  }
+
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
